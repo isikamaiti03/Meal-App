7d41a149e44513a12ed19101551dcd61cdcd774e
Isika Maiti <it2027@cemk.ac.in>
2024-09-19 12:30:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 
 import React from "react";
 import { Routes, Route} from "react-router-dom";
-// import './App.css';
-// import Practice from "./WeeKPractice/Practice";
-// import FormData from "./WeeKPractice/FormData";
-// import DataForm from "./WeeKPractice/DataForm";
-import Meal from "./WeeKPractice/MEAL FOLDER/Meal";
+import Meal from "./MEAL FOLDER/Meal";
 
 const App = () => {
- 
   return (
     <>
-    
     <Routes>
-      {/* <Route path="/" element={<Practice/>} />
-      <Route path="/form" element={<FormData/>}/>
-      <Route path="/form1" element={<DataForm/>}/> */}
       <Route path="/" element={<Meal/>}/>
     </Routes>
    </> 
